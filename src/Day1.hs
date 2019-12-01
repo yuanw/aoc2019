07f071983fe14696a5e26a93c7220c82ce18b706
Yuan Wang <me@yuanwang.cam>
2019-12-01 10:35:34 -0600

    day1

---
+++ src/Day1.hs
@@ -0,0 +1,18 @@
+module Day1 where
+
+fuel :: Int -> Int
+fuel x = (x `div` 3) - 2
+
+readInput :: IO [Int]
+readInput = do
+  content <- readFile "input/day1.txt"
+  return $ map read $ lines content
+
+result :: IO [Int] -> IO Int
+result = fmap (sum . map fixFuel)
+
+--TODO using fix point, at least write as tail recursion
+-- https://medium.com/@cdsmithus/fixpoints-in-haskell-294096a9fc10
+fixFuel :: Int -> Int
+fixFuel x = if y > 6 then y + fixFuel y  else y
+  where y = fuel x
